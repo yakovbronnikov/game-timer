89141c7d0625c9c7158a06d3972ccc8f4471acfb
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2025-02-11 12:41:08 +1000

    fix: sw

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// SW
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
+
+// 
+
 let timerElement = document.getElementById('timer')
 let timerButton = document.querySelector('.round-timer')
 let startButton = document.querySelector('.round-start')
